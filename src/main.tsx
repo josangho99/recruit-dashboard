30187bbc2adcedcbe8f12c367829bb610a2dab33
Josangho99 <josangho99@gmail.com>
2025-11-19 00:10:58 +0900

    feat: add react-route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "@/pages/Layout";
+import Dashboard from "@/components/dashboard/Dashboard";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
