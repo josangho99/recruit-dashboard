ea55a59502dfe6ae4ce6483a491b2580617ca308
Josangho99 <josangho99@gmail.com>
2025-11-21 02:46:10 +0900

    fix: remove import React

--- src/components/dashboard/Linechart.tsx
+++ src/components/dashboard/Linechart.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import type { PaymentItem } from "@/interfaces/payment.interface";
 import {
   Chart as ChartJS,
--- src/components/dashboard/PieChart.tsx
+++ src/components/dashboard/PieChart.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import type { PaymentItem, PayTypeCount } from "@/interfaces/payment.interface";
