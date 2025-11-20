94cc5f8d51296815404366c2a26826756bf7d98e
Josangho99 <josangho99@gmail.com>
2025-11-21 02:46:47 +0900

    update: remove search input

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,6 @@ function Header({ title }: { title: string }) {
   return (
     <div className="flex flex-row items-center justify-between p-4">
       <span className="text-3xl font-bold">{title}</span>
-      <input className="rounded-2xl bg-white px-4 py-1.5" type="text" placeholder="Search"></input>
     </div>
   );
 }
